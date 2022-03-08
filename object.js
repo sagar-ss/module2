05f25a10d7026d5ef1b37fcf0a6aac247259bcb8
sagar-ss <sagarsrivastava0302@gmail.com>
2022-03-08 22:20:28 +0530

    object CapAmerica

---
+++ object.js
@@ -0,0 +1,30 @@
+//empty object
+let obj={};
+console.log(obj);
+
+// object is key value pair and key is always string
+
+let personal={
+    "First_Name":"Sagar",
+    "Age":45,
+    "last_Name":"Srivastava",
+    //⭐without double quotes
+    phone:9887980150
+};
+console.log(personal);
+
+let capAmerica={
+    "Name":"Steve",
+    Age:150,
+    Friend:['natasha','buckky','bruce'],
+    Address:{
+        State:'manhatan',
+        City:'haryana'
+    },
+    sayHi:function(){
+        console.log('hello from Captain');
+    }
+};
+
+console.log(capAmerica);
+
